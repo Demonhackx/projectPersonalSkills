b226423b4355047de5f38a2a59a0bf3e8f08756d
Valerio <103501448+Demonhackx@users.noreply.github.com>
2024-02-16 15:37:23 +0100

    First commit

---
+++ projectPersonalSkills/codingSkills/src/data.js
@@ -0,0 +1,54 @@
+const data = [
+  {
+    id: 1,
+    nome: "Javascript",
+    stato: "Programming language used for web development; executed on the client's browser to add interactivity to web pages.",
+    img:
+      "https://cdn.pixabay.com/photo/2015/04/23/17/41/javascript-736400_1280.png",
+  },
+  {
+    id: 2,
+    nome: "Typescript",
+    stato: "Superset of JavaScript with static types; compiled to JavaScript for execution in browsers or servers.",
+    img:
+      "https://res.cloudinary.com/dn7or1s0g/image/upload/v1708083604/bgwpumy1i6cmksncaz5s.png",
+  },
+  {
+    id: 3,
+    nome: "Angular v.17",
+    stato: "Front-end framework for creating SPAs and PWAs; offers features like two-way data binding and dependency injection.",
+    img:
+      "https://res.cloudinary.com/dn7or1s0g/image/upload/v1708083692/fevkce2jhkeq0emnovvs.jpg",
+  },
+  {
+    id: 4,
+    nome: "React.js",
+    stato:
+      "Library for creating dynamic and responsive UIs, based on reusable component architecture.",
+    img:
+      "https://res.cloudinary.com/dn7or1s0g/image/upload/v1708083785/qcz0jdwwfeigzrce7v0n.svg",
+  },
+  {
+    id: 5,
+    nome: "Node.js",
+    stato: "JavaScript-based runtime environment for running server-side code; scalable and high-performance.",
+    img:
+      "https://res.cloudinary.com/dn7or1s0g/image/upload/v1708083899/u72phsaqpq4w2zz8t57w.png",
+  },
+  {
+    id: 6,
+    nome: "Material-UI (MUI)",
+    stato: "React library implementing Google's Material Design principles for creating elegant and intuitive UIs, providing pre-built components and customizable styles.",
+    img:
+      "https://res.cloudinary.com/dn7or1s0g/image/upload/v1708085414/shqywbrw6v9he3ucagti.png",
+  },
+  {
+    id: 7,
+    nome: "Bootstrap",
+    stato: "Open-source front-end framework for creating responsive and visually appealing user interfaces, based on HTML, CSS, and JavaScript.",
+    img:
+      "https://res.cloudinary.com/dn7or1s0g/image/upload/v1708085354/kz1ecvsfqb4uats50ybm.png",
+  },
+];
+
+export default data;
